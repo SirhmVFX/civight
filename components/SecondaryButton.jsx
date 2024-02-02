59888ceb7658dd63a2ae818f70e0027138622a9d
sirhmvfx <sirhmvfx@gmail.com>
2024-02-03 00:27:54 +0100

    working

---
+++ components/SecondaryButton.jsx
@@ -0,0 +1,9 @@
+function SecondaryButton({ label }) {
+  return (
+    <button className="bg-secondarycolor text-white rounded-full w-full p-4">
+      {label}
+    </button>
+  );
+}
+
+export default SecondaryButton;
