92c3e3fa5fa8e5b654830eaa249892fca389d82d
sirhmvfx <sirhmvfx@gmail.com>
2024-02-02 22:09:29 +0100

    working

---
+++ app/(auth)/splash2/page.jsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
+
+function Splash2() {
+  return (
+    <>
+      <section className="w-full h-screen p-8">
+        <div className="w-full md:3/4 mx-auto flex flex-col p-8 gap-20">
+          <Image src={"/images/img2.png"} width={550} height={550} alt="img2" />
+
+          <div>
+            <h1 className="text-7xl"> Manage & Secure your Yourself</h1>
+            <p>Stay Safe and Secured everywhere you go</p>
+          </div>
+
+          <Link href={"/splash3"} className="button">
+            Next
+          </Link>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Splash2;
