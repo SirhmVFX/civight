e5a871afd9373c2d362175f04f17399e3753202e
sirhmvfx <sirhmvfx@gmail.com>
2024-02-03 15:07:53 +0100

    workijg on main app

---
+++ components/Panic.jsx
@@ -0,0 +1,14 @@
+function Panic({ option, icon }) {
+  return (
+    <>
+      <div className="border-2 border-primarycolor p-2 rounded-full">
+        <div className="w-[100px] h-[100px] bg-secondarycolor roudned-full flex flex-col items-center justify-center rounded-full">
+          <div>{icon}</div>
+          <p className="text-xl text-primarycolor">{option}</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Panic;
