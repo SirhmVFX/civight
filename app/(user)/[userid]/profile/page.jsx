25dc5b7c9b18025f6ec0365063426bcc345a5d9b
sirhmvfx <sirhmvfx@gmail.com>
2024-02-04 16:22:24 +0100

    alomost done with the ui

--- app/(user)/[userid]/profile/page.jsx
+++ app/(user)/[userid]/profile/page.jsx
@@ -291,7 +291,7 @@ function Profile() {
                   />
                 </svg>
 
-                <Link href="/">Log Out s</Link>
+                <Link href="/">Log Out</Link>
               </div>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
