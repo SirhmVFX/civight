7f7573b21e2f6bbd8289d7ff9b7b0cf23b611e00
sirhmvfx <sirhmvfx@gmail.com>
2024-02-05 16:37:51 +0100

    alomost done with the ui

---
+++ app/(user)/[userid]/facialrecognition/page.jsx
@@ -0,0 +1,22 @@
+import { CiCamera } from "react-icons/ci";
+
+function FacialRecognition() {
+  return (
+    <>
+      <section className="w-full h-screen p-8">
+        <div className="w-full md:w-3/4 flex flex-col items-center">
+          <div className="border border-dashed rounded-lg flex w-full h-[400px] justify-center items-center">
+            <CiCamera className="text-5xl" />
+          </div>
+
+          <div className="flex flex-col items-center gap-4">
+            <h1>Facial Recognition</h1>
+            <p>Scan a face to find a record</p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default FacialRecognition;
