d054e039f714448a2f230ad9ca26489b13376d89
sirhmvfx <sirhmvfx@gmail.com>
2024-02-05 18:12:48 +0100

    alomost done with the ui

--- app/(user)/[userid]/facialrecognition/page.jsx
+++ app/(user)/[userid]/facialrecognition/page.jsx
@@ -6,16 +6,16 @@ function FacialRecognition() {
     <>
       <section className="w-full h-screen p-8">
         <div className="w-full md:w-3/4 flex flex-col items-center">
-          <div className="border border-dashed rounded-lg flex w-full h-[400px] justify-center items-center">
+          <div className="border border-dashed border-gray-500 rounded-lg flex w-full h-[400px] justify-center items-center">
             <CiCamera className="text-5xl" />
           </div>
 
-          <div className="flex flex-col items-center gap-4">
-            <h1>Facial Recognition</h1>
+          <div className="flex flex-col items-center gap-2 mt-8">
+            <h1 className="text-3xl font-bold">Facial Recognition</h1>
             <p>Scan a face to find a record</p>
           </div>
 
-          <div className="mt-32 w-full">
+          <div className="mt-28 w-full">
             <SecondaryButton label={"Scan"} />
           </div>
         </div>
