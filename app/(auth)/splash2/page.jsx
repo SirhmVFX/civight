8c605c53c14e4f382fb42740c1dfda84c95422b9
sirhmvfx <sirhmvfx@gmail.com>
2024-02-06 12:21:18 +0100

    working on auth

--- app/(auth)/splash2/page.jsx
+++ app/(auth)/splash2/page.jsx
@@ -5,7 +5,7 @@ function Splash2() {
   return (
     <>
       <section className="w-full h-screen p-8">
-        <div className="w-full md:3/4 mx-auto flex flex-col p-8 gap-20">
+        <div className="w-full md:3/4 mx-auto flex flex-col gap-20">
           <Image src={"/images/img2.png"} width={550} height={550} alt="img2" />
 
           <div>
