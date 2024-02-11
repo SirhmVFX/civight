0015c3ea280e73fe6bf8e0313454f7f0c4246985
sirhmvfx <sirhmvfx@gmail.com>
2024-02-11 02:03:04 +0100

    finishing functionalities

--- app/(user)/[userid]/anonymoustip/page.jsx
+++ app/(user)/[userid]/anonymoustip/page.jsx
@@ -31,6 +31,34 @@ function AnonymousTip() {
     const uploadImage = () => {
       const filename = new Date().getTime() + file.name;
       const storageRef = ref(storage, filename);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+          }
+        },
+        (error) => {
+          console.log(error);
+        },
+        () => {
+          // Handle successful uploads on complete
+          // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            console.log("File available at", downloadURL);
+          });
+        }
+      );
     };
 
     file && uploadImage();
